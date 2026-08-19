7c18ee1578a31fd421f538a352daea73b94d19ce
macclary51-prog <macclary51@gmail.com>
2026-08-19 10:45:19 -0700

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDACoQU74FIxuCApDJxQUP-rcXhsDIorAs",
+  authDomain: "business-toolbox-c938a.firebaseapp.com",
+  projectId: "business-toolbox-c938a",
+  storageBucket: "business-toolbox-c938a.firebasestorage.app",
+  messagingSenderId: "353839785877",
+  appId: "1:353839785877:web:b35f3d7fb738848edd389c",
+  measurementId: "G-RBGXYYGTQ9"
+};
